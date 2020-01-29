f8f1920ff82c7e43b7c88c5e50df398f16a41563
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-01-29 17:25:47 +0200

    Faker

---
+++ SocialNetwork/SocialNetwork.Faker/Fake/FakePost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bogus;
+using SocialNetwork.Dal.Models;
+
+
+namespace SocialNetwork.Fake.Fake
+{
+    class FakePost
+    {
+        public readonly Faker<Post> _fakePost;
+        public FakePost()
+        {
+
+        }
+        public User Generate()
+        {
+            return _fakePost.Generate();
+        }
+
+    }
+
+    
+}
