f7248696ca32701a32a1c7f752de94611a17c5d4
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-01-29 19:15:15 +0200

    test

--- SocialNetwork/SocialNetwork.Fake/Program.cs
+++ SocialNetwork/SocialNetwork.Fake/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Bogus;
+using SocialNetwork.Dal.Models;
 
 namespace SocialNetwork.Fake
 {
@@ -7,6 +10,28 @@ namespace SocialNetwork.Fake
         static void Main(string[] args)
         {
             
+        }
+
+    }
+
+    public class FakeUser
+    {
+        public Faker<User> _fakeUser;
+        public FakeUser()
+        {
+            var fakePost = new Faker<Post>()
+                .RuleFor(x => x.Text, x => x.Lorem.Text())
+                .RuleFor(x => x.ImgUrl, x => x.Internet.Avatar())
+                .RuleFor(x => x.Date, x => x.Date.Recent(12));
+                //.RuleFor(x => x.PostComment, x => x.)
+
+            var fakeUserPost = new Faker<UserPost>()
+                .RuleFor(x => x.Post, x => fakePost.Generate(1).First());
+
+            _fakeUser = new Faker<User>()
+                .RuleFor(x => x.Name, x=> x.Name.FullName())
+                .RuleFor(x => x.UserPost, x => fakeUserPost.Generate(10).ToList())
+                .RuleFor()
         }
     }
 }
