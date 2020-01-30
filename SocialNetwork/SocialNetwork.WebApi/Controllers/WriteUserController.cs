a59c56f5c69c8066194982017d56cb6c079ad70a
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-01-30 18:18:44 +0200

    writeuser

---
+++ SocialNetwork/SocialNetwork.WebApi/Controllers/WriteUserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SocialNetwork.Dal.Context;
+using SocialNetwork.Dal.Models;
+
+namespace SocialNetwork.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WriteUserController : ControllerBase
+    {
+        public PublicContext context;
+        private readonly ILogger<WriteUserController> _logger;
+        public WriteUserController(ILogger<WriteUserController> logger)
+        {
+            _logger = logger;
+            context = new PublicContext();
+        }
+
+        [HttpGet]
+        public User Get(string name)
+        {
+            Guid id = new Guid();
+            User user = new User { Name = name, Id = id };
+            Console.WriteLine(user.Name);
+            context.User.Add(user);
+            context.SaveChanges();
+            return user;
+        }
+    }
+}
