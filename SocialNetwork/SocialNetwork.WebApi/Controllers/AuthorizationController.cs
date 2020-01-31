347aa2a95419f3e3a3c1157f7601f703c0b1b18a
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-01-31 16:30:03 +0200

    auth

--- SocialNetwork/SocialNetwork.WebApi/Controllers/AuthorizationController.cs
+++ SocialNetwork/SocialNetwork.WebApi/Controllers/AuthorizationController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SocialNetwork.Dal.Context;
 using SocialNetwork.Dal.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 
 namespace SocialNetwork.WebApi.Controllers
@@ -22,16 +25,52 @@ namespace SocialNetwork.WebApi.Controllers
             context = new PublicContext();
         }
 
-        /*[HttpGet]
-        public string GetToken(string name, string password)
+        [HttpGet]
+        public string Get(string username, string password)
         {
-            User user = context.User.Where(x => x.Name == name).FirstOrDefault();
-            UserSecurity usersecurity = context.UserSecurity.Where(x => x.UserId == user.Id).FirstOrDefault();
-            if (usersecurity.Password == password)
+            var identity = GetIdentity(username, password);
+            if (identity == null)
             {
-                string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
-                return token;
+                return BadRequest(new { errorText = "Invalid username or password." });
             }
-        }*/
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Json(response);
+        }
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
     }
 }
