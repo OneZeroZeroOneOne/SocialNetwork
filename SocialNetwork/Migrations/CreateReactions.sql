bc8dee003918de051ad751fe5222d33b748f3101
Forevka <zebestforevka@gmail.com>
2020-02-04 20:39:17 +0200

    updated types and added constrints

--- SocialNetwork/Migrations/CreateReactions.sql
+++ SocialNetwork/Migrations/CreateReactions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "ReactionTypeComment" (
-  "ReactionId" serial NOT NULL,
+  "ReactionId" uuid NOT NULL,
   "Content" TEXT NOT NULL,
   CONSTRAINT "ReactionTypeComment_pk" PRIMARY KEY ("ReactionId")
 ) WITH (
@@ -9,7 +9,7 @@ CREATE TABLE "ReactionTypeComment" (
 
 
 CREATE TABLE "ReactionTypePost" (
-  "ReactionId" serial NOT NULL,
+  "ReactionId" uuid NOT NULL,
   "Content" TEXT NOT NULL,
   CONSTRAINT "ReactionTypePost_pk" PRIMARY KEY ("ReactionId")
 ) WITH (
@@ -19,7 +19,7 @@ CREATE TABLE "ReactionTypePost" (
 
 
 CREATE TABLE "ReactionComment" (
-  "ReactionId" serial NOT NULL,
+  "ReactionId" uuid NOT NULL,
   "UserId" uuid NOT NULL,
   "CommentId" uuid NOT NULL,
   CONSTRAINT "ReactionComment_pk" PRIMARY KEY ("ReactionId","UserId")
@@ -30,13 +30,17 @@ CREATE TABLE "ReactionComment" (
 
 
 CREATE TABLE "ReactionPost" (
-  "ReactionId" serial NOT NULL,
+  "ReactionId" uuid NOT NULL,
   "UserId" uuid NOT NULL,
   "PostId" uuid NOT NULL,
   CONSTRAINT "ReactionPost_pk" PRIMARY KEY ("ReactionId","UserId")
 ) WITH (
   OIDS=FALSE
 );
+
+ALTER TABLE "ReactionComment" ADD CONSTRAINT "ReactionToComment_fk0" FOREIGN KEY ("ReactionId") REFERENCES "Comment"("Id");
+ALTER TABLE "ReactionPost" ADD CONSTRAINT "ReactionToPost_fk0" FOREIGN KEY ("PostId") REFERENCES "Post"("Id");
+
 
 ALTER TABLE "ReactionComment" ADD CONSTRAINT "ReactionComment_fk0" FOREIGN KEY ("ReactionId") REFERENCES "ReactionTypeComment"("ReactionId");
 ALTER TABLE "ReactionComment" ADD CONSTRAINT "ReactionComment_fk1" FOREIGN KEY ("UserId") REFERENCES "User"("Id");
