73906c46fe35e2a76467c1a19769293b14a7ba3b
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-02-13 12:48:41 +0200

    separated

--- SocialNetwork.WebUI/src/components/PostComponent.vue
+++ SocialNetwork.WebUI/src/components/PostComponent.vue
@@ -33,14 +33,6 @@
         ERROR
       </div>
     </div>
-      <ul id="comments">
-        <li v-for="item in commentObjs" v-bind:key="item.id">
-          <CommentComponent :commentObj="item"/>
-        </li>
-      </ul>
-    <div class="footer-end">
-
-    </div>
   </div>
 
 </template>
@@ -50,7 +42,6 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import { Guid } from "@/utilities/guid";
 import CommentComponent from "./CommentComponent.vue";
 import { PostService } from "@/services/PostService";
-import { CommentService } from "@/services/CommentService";
 import { IPost } from "@/models/responses/PostViewModel";
 import { ResponseState } from "@/models/enum/ResponseState";
 import { IPagedResult } from '../models/responses/PagedResult';
@@ -58,45 +49,19 @@ import { IComment } from '../models/responses/CommentViewModel';
 import Nprogress from "nprogress"
 import _ from 'lodash'
 
-@Component({
-  components: { 
-    CommentComponent
-  }
-})
+@Component({})
 export default class PostComponent extends Vue {
   @Prop() public postId!: string;
   private postObj!: IPost; 
-
-  private commentObjs: IComment[] = [];
-  private commentIds: Guid[] = [];
-  private currentPage: number = 1;
-
   private requestPostStatus: ResponseState = ResponseState.loading;
-  private requestCommentsStatus: ResponseState = ResponseState.loading;
-
-  private scrolledToBottom: boolean = false;
 
   constructor() {
     super();
     Nprogress.start()
     this.loadPost();
-    Nprogress.set(0.5)
-    this.loadComments();
     Nprogress.done();
-
-    window.onscroll = () => {
-      let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
-      if (bottomOfWindow) {
-        this.scrolledToBottom = true // replace it with your code
-        console.log("scrolled to bottom")
-        this.throttleLoadComments();
-      }
-    }
-
-    setInterval(() => this.loadComments(), 30000);
   }
   
-  throttleLoadComments = _.throttle(() => this.loadComments(), 2000);
 
   async loadPost(): Promise<void> {
     this.requestPostStatus = ResponseState.loading;
@@ -110,46 +75,6 @@ export default class PostComponent extends Vue {
         this.requestPostStatus = ResponseState.fail;
       });
   }
-
-  async loadComments()
-  {
-    console.log("loading comments")
-    this.requestCommentsStatus = ResponseState.loading;
-    Nprogress.start()
-
-    CommentService.getCommentForPost(this.postId, this.currentPage, 6)
-      .then(response => {
-        if (response.currentPage < response.pageCount)
-        {
-          this.currentPage += 1;
-        }
-        this.requestCommentsStatus = ResponseState.success;
-        let newComCount: number = 0;
-        response.results.forEach(x => {
-          if (this.commentIds.indexOf(x.id) === -1)
-          {
-            this.commentIds.push(x.id);
-            this.commentObjs.push(x);
-            newComCount += 1;
-          }
-        })
-        Nprogress.done();
-        console.log(this.commentIds);
-        console.log(this.commentObjs)
-        this.$notify({
-          group: 'foo',
-          title: 'Loaded comments',
-          text: newComCount === 0 ? 
-            'No new comments' : 
-            'Loaded ' + newComCount.toString() + " comments",
-        });
-      })
-      .catch(error => {
-        this.requestCommentsStatus = ResponseState.fail;
-        Nprogress.done();
-      })
-  }
-
 }
 </script>
 
@@ -204,12 +129,5 @@ $text-color: #ccc;
     border-top-width: 2px;
     border-top-color: cornflowerblue;
   }
-
-}
-
-#comments {
-  width: 80%;
-  float: left;
-  margin-left: 40px;
 }
 </style>
--- SocialNetwork.WebUI/src/views/PostView.vue
+++ SocialNetwork.WebUI/src/views/PostView.vue
@@ -1,28 +1,118 @@
 <template>
   <div class="home">
-    <PostComponent :postId="postId"/>
+    <PostComponent :postId="postId()"/>
+    <ul id="comments">
+      <li v-for="item in commentObjs" v-bind:key="item.id">
+        <CommentComponent :commentObj="item"/>
+      </li>
+    </ul>
+    <div class="footer-end">
+
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
+import { Component, Prop, Vue } from "vue-property-decorator";
 import PostComponent from '@/components/PostComponent.vue'
+import CommentComponent from "@/components/CommentComponent.vue";
+import { ResponseState } from "@/models/enum/ResponseState";
+import { IPagedResult } from '../models/responses/PagedResult';
+import { IComment } from '../models/responses/CommentViewModel';
+import { Guid } from "@/utilities/guid";
+import { PostService } from "@/services/PostService";
+import { CommentService } from "@/services/CommentService";
+import Nprogress from "nprogress"
+import _ from 'lodash'
 
-export default {
-  name: 'PostView',
-  components: {
+@Component({
+  components: { 
+    CommentComponent,
     PostComponent
-  },
-  computed: {
-    // a computed getter
-    postId: function () {
-      // `this` points to the vm instance
-      console.log(this.$route)
-      if (this.$route.query.hasOwnProperty('id'))
-        return this.$route.query.id
-      return 'error'
+  }
+})
+export default class PostView extends Vue {
+  private requestCommentsStatus: ResponseState = ResponseState.loading;
+
+  private commentObjs: IComment[] = [];
+  private commentIds: Guid[] = [];
+  private currentPage: number = 1;
+
+  private scrolledToBottom: boolean = false;
+
+  constructor() {
+    super();
+    window.onscroll = () => {
+      let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight >= document.documentElement.offsetHeight - 100
+      if (bottomOfWindow) {
+        this.scrolledToBottom = true // replace it with your code
+        console.log("scrolled to bottom")
+        this.throttleLoadComments();
+      }
     }
+
+    setInterval(() => this.loadComments(), 30000);
+
+    this.loadComments()
+  }
+
+  throttleLoadComments = _.throttle(() => this.loadComments(), 2000);
+
+  postId(): string {
+    console.log(this.$route)
+    if (this.$route.query.hasOwnProperty('id'))
+      return this.$route.query.id.toString()
+    return 'error'
+  }
+
+  async loadComments()
+  {
+    console.log("loading comments")
+    this.requestCommentsStatus = ResponseState.loading;
+    Nprogress.start()
+
+    CommentService.getCommentForPost(this.postId(), this.currentPage, 50)
+      .then(response => {
+        if (response.currentPage < response.pageCount)
+        {
+          this.currentPage += 1;
+        }
+        this.requestCommentsStatus = ResponseState.success;
+        let newComCount: number = 0;
+        response.results.forEach(x => {
+          if (this.commentIds.indexOf(x.id) === -1)
+          {
+            this.commentIds.push(x.id);
+            this.commentObjs.push(x);
+            newComCount += 1;
+          }
+        })
+        Nprogress.done();
+        console.log(this.commentIds);
+        console.log(this.commentObjs)
+        this.$notify({
+          group: 'foo',
+          title: 'Loaded comments',
+          text: newComCount === 0 ? 
+            'No new comments' : 
+            'Loaded ' + newComCount.toString() + " comments",
+        });
+      })
+      .catch(error => {
+        this.requestCommentsStatus = ResponseState.fail;
+        Nprogress.done();
+      })
   }
 }
 </script>
 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#comments {
+  width: 80%;
+  float: left;
+  margin-left: 40px;
+}
+</style>
