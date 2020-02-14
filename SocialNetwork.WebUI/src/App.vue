93139b7f5881e22bbe8ddc6e9ac3f51a0061c9d6
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-02-14 14:18:59 +0200

    footer component

--- SocialNetwork.WebUI/src/App.vue
+++ SocialNetwork.WebUI/src/App.vue
@@ -3,11 +3,32 @@
     <div id="nav"></div>
     <router-view/>
     <notifications group="foo" position="bottom right"/>
+    <!--<FooterComponent/>-->
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+@Component({
+  components: {
+    FooterComponent
+  }
+})
+export default class App extends Vue {
+  constructor() {
+    super();
+  }
+}
+</script>
 
 <style lang="scss">
 html {
   background: url('assets/background-tiled-memphis.png');
+}
+
+#app {
+  display: relative;
 }
 </style>
---
+++ SocialNetwork.WebUI/src/components/FooterComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">This footer will always be positioned at the bottom of the page, but <strong>not fixed</strong>.</footer>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Guid } from "@/utilities/guid";
+import { ResponseState } from "@/models/enum/ResponseState";
+import Nprogress from "nprogress"
+import _ from 'lodash'
+
+@Component({})
+export default class FooterComponent extends Vue {
+  constructor() {
+    super();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.footer {
+  position: relative;
+  text-align: center;
+  width:100%;
+  height:60px;			/* Height of the footer */
+  background:#6cf;
+}
+</style>
