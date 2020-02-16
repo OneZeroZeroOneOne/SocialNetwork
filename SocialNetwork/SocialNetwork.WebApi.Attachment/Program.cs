4182945d919540740c43de7903c1b99a65d2df68
Forevka <zebestforevka@gmail.com>
2020-02-16 20:56:42 +0200

    test deploy

--- SocialNetwork/SocialNetwork.WebApi.Attachment/Program.cs
+++ SocialNetwork/SocialNetwork.WebApi.Attachment/Program.cs
@@ -14,6 +14,7 @@ namespace SocialNetwork.WebApi.Attachment
         }
 
 
+
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
