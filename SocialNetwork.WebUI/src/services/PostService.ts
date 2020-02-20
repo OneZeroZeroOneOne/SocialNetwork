6c9d97cc7e0a016928e1f82c9081886767a3f974
Forevka <zebestforevka@gmail.com>
2020-02-20 21:02:04 +0200

    fix

--- SocialNetwork.WebUI/src/services/PostService.ts
+++ SocialNetwork.WebUI/src/services/PostService.ts
@@ -7,11 +7,11 @@ export class PostService {
     private static postsAxios = axios.create();
 
     static async getPost(board_id: Guid, post_id: string): Promise<IPost> {
-        let url = 'http://16ch.tk/social/Posts/' + board_id.toString() + "/" + post_id;
+        let url = 'http://16ch.tk/api/social/Posts/' + board_id.toString() + "/" + post_id;
         return (await this.postsAxios.get<IPost>(url)).data;
     }
     static async getNewPosts(user_id: string, page: string, quantity: string): Promise<IPagedResult<IPost>> {
-        let url = 'http://16ch.tk/social/Post/Page/' + "?page=" + page + "?page=" + quantity;
+        let url = 'http://16ch.tk/api/social/Post/Page/' + "?page=" + page + "?page=" + quantity;
         return (await this.postsAxios.get<IPagedResult<IPost>>(url)).data;
     }
 }
