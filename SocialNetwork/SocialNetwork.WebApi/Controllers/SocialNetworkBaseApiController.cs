681bd6b0d4abe8cd971bae043adeb22359e4aacb
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-03-02 17:22:11 +0200

    revert

--- SocialNetwork/SocialNetwork.WebApi/Controllers/SocialNetworkBaseApiController.cs
+++ SocialNetwork/SocialNetwork.WebApi/Controllers/SocialNetworkBaseApiController.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Security.Claims;
+using SocialNetwork.Utilities.Exceptions;
 
 namespace SocialNetwork.WebApi.Controllers
 {
     public class SocialNetworkBaseApiController : ControllerBase
     {
-        public static readonly Guid AnonimUser = new Guid("37f56076-4eff-400d-8ba0-0c8b37491182");
-
         [ApiExplorerSettings(IgnoreApi = true)]
         public Guid CurrentUser()
         {
             if (Guid.TryParse(User.FindFirst(ClaimsIdentity.DefaultNameClaimType).Value, out Guid currentUserId))
                 return currentUserId;
 
-            return AnonimUser;
+            throw ExceptionFactory.SoftException(ExceptionEnum.UserNotFound, "User not found while getting currentUser");
         }
     }
 }
