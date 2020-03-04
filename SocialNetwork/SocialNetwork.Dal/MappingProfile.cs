d04a34678460cecc2e459a35fd107bacb46f1a5d
Forevka <zebestforevka@gmail.com>
2020-03-05 00:00:55 +0200

    fix for attachments in posts

--- SocialNetwork/SocialNetwork.Dal/MappingProfile.cs
+++ SocialNetwork/SocialNetwork.Dal/MappingProfile.cs
@@ -41,7 +41,11 @@ namespace SocialNetwork.Dal
             CreateMap<AttachmentPost, OutAttachmentViewModel>()
                 .ForMember(x => x.Id, y => y.MapFrom(x => x.Attachment.Id))
                 .ForMember(x => x.Path, y => y.MapFrom(x => x.Attachment.Path))
-                .ForMember(x => x.Preview, y => y.MapFrom(x => x.Attachment.Preview));
+                .ForMember(x => x.Preview, y => y.MapFrom(x => x.Attachment.Preview))
+                .ForMember(x => x.DisplayName, y => y.MapFrom(x => x.Attachment.DisplayName))
+                .ForMember(x => x.Width, y => y.MapFrom(x => x.Attachment.Width))
+                .ForMember(x => x.Height, y => y.MapFrom(x => x.Attachment.Height));
+
 
             CreateMap<EditCommentViewModel, Comment>()
                 .ForMember(destinationMember => destinationMember.Id, sourceMember => sourceMember.MapFrom(x => x.CommentId));
