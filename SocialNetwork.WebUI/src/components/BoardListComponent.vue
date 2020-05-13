f05cd7b3654fe3641dfd69d1da03afdf70e87d42
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-05-13 20:26:58 +0300

    better board select view

--- SocialNetwork.WebUI/src/components/BoardListComponent.vue
+++ SocialNetwork.WebUI/src/components/BoardListComponent.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="board-list">
-    <div class="board-list-container" v-if="boardRequestStatus === 1">
-      <card v-for="item in boards" 
-      v-bind:key="item.id"
-      :link-to="item.name"
-      :data-images="getBoardPreview(item)">
-        <h1 slot="header">/{{item.name}}/</h1>
-        <p slot="content">{{item.description}}</p>
-      </card>
+    <div class="container-wrapper popular">
+      <div class="board-list-container" v-if="boardRequestStatus === 1">
+        <card v-for="item in popularBoards" 
+        v-bind:key="item.id"
+        :link-to="item.name"
+        :data-images="getBoardPreview(item)">
+          <h1 slot="header">/{{item.name}}/</h1>
+          <p slot="content">{{item.description}}</p>
+        </card>
+      </div>
+    </div>
+    <div class="container-wrapper other">
+      <div class="thematics">
+        Тематика
+        <span v-for="item in thematicsBoards" v-bind:key="item.id" v-on:click="$router.push(item.name)">
+          <span class='link'>/{{item.name}}/</span> - {{item.description}}
+        </span>
+      </div>
+      <div class="politicians">
+        Политика и новости
+        <span v-for="item in politiciansBoards" v-bind:key="item.id" v-on:click="$router.push(item.name)">
+          <span class='link'>/{{item.name}}/</span> - {{item.description}}
+        </span>
+      </div>
+      <div class="non">
+        Прочее
+        <span v-for="item in otherBoards" v-bind:key="item.id" v-on:click="$router.push(item.name)">
+          <span class='link'>/{{item.name}}/</span> - {{item.description}}
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +39,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import { ResponseState } from "@/models/enum/ResponseState";
 import { Guid } from "@/utilities/guid";
 
-import { IBoard } from "@/models/responses/Board";
+import { IBoard, IBoards } from "@/models/responses/Board";
 
 import { IBoardService } from '@/services/Abstractions/IBoardService';
 
@@ -39,6 +61,12 @@ export default class BoardListComponent extends Vue {
 
   private _boardService!: IBoardService;
 
+  private popularBoards: IBoard[] = [];
+
+  private thematicsBoards: IBoard[] = [];
+  private politiciansBoards: IBoard[] = [];
+  private otherBoards: IBoard[] = [];
+
   constructor() {
     super();
     this.loadBoards();
@@ -59,10 +87,36 @@ export default class BoardListComponent extends Vue {
       .then(response => {
         this.boards = response.data;
         this.boardRequestStatus = ResponseState.success;
+        this.sortBoards()
       })
       .catch(error => {
         this.boardRequestStatus = ResponseState.fail;
       });
+  }
+
+  sortBoards() {
+    this.popularBoards = []
+    this.thematicsBoards = []
+    this.politiciansBoards = []
+    this.otherBoards = []
+
+    this.boards.forEach(board => {
+      let popSetting = board.settings.filter(x => x.name === 'Popular')
+      if (popSetting.length > 0)
+        this.popularBoards.push(board)
+
+      let thematicsSettings = board.settings.filter(x => x.name === 'Thematic')
+      if (thematicsSettings.length > 0)
+        this.thematicsBoards.push(board)
+
+      let politicsSettings = board.settings.filter(x => x.name === 'Politicians')
+      if (politicsSettings.length > 0)
+        this.politiciansBoards.push(board)
+
+      let otherSettings = board.settings.filter(x => x.name === 'Other')
+      if (otherSettings.length > 0)
+        this.otherBoards.push(board)
+    })
   }
 }
 </script>
@@ -72,6 +126,53 @@ export default class BoardListComponent extends Vue {
 $board-name-color: #ba6b57;
 $board-description-color: #ffa41b;
 $board-hover-color: #e7b2a5;
+
+.board-list {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.container-wrapper {
+  background-color: #192734;// rgb(56, 67, 78);
+  display: inline-block;
+  border-color: rgb(56, 67, 78);
+  border-style: solid;
+  border-width: 1px;
+  padding: 12px;
+  &.other {
+    text-align: left;
+    margin-top: 15px;
+    display: flex;
+    column-gap: 30px;
+    justify-content: space-between;
+
+    span:first-child {
+      padding-top: 10px !important;
+    }
+
+    span:hover {
+      cursor: pointer;
+      .link {
+        color: chocolate;
+      }
+    }
+
+    span:not(.link) {
+      padding: 1px;
+    }
+
+    div {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      margin-left:10px;
+      margin-right:10px;
+    }
+    .link {
+      color: coral;
+    }
+  }
+}
 
 .board-list-container {
   //padding: 40px 80px;
