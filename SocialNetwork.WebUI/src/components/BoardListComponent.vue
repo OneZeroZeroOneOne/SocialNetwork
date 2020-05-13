9df57a2bb27620f20f845d61d30967bc82448fea
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-05-13 13:47:09 +0300

    updated list of boards

--- SocialNetwork.WebUI/src/components/BoardListComponent.vue
+++ SocialNetwork.WebUI/src/components/BoardListComponent.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="board-list">
-        <div class="board-list-container" v-if="boardRequestStatus === 1">
-        <li v-for="item in boards" v-bind:key="item.id" class="board-link">
-            <span @click="$router.push(item.name)">
-                <div class="board-name">/{{item.name}}/</div> - <div class="board-description">{{item.description}}</div>
-            </span>
-        </li>
-        </div>
+  <div class="board-list">
+    <div class="board-list-container" v-if="boardRequestStatus === 1">
+      <card v-for="item in boards" 
+      v-bind:key="item.id"
+      :link-to="item.name"
+      :data-images="['http://img0.joyreactor.cc/pics/post/art-%D0%BE%D0%BB%D0%B4%D1%84%D0%B0%D0%B3-2ch-4699278.jpeg', 'https://2ch.hk/char/src/2777/14717699796150.jpg']">
+        <h1 slot="header">/{{item.name}}/</h1>
+        <p slot="content">{{item.description}}</p>
+      </card>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,11 +22,16 @@ import { IBoard } from "@/models/responses/Board";
 import { IBoardService } from '@/services/Abstractions/IBoardService';
 
 import { BoardService } from '../services/Implementations/BoardService';
+import Card from '@/components/BoardCardComponent.vue';
 
 import Nprogress from "nprogress"
 import _ from 'lodash'
 
-@Component({})
+@Component({
+  components: { 
+    Card,
+  }
+})
 export default class BoardListComponent extends Vue {
   private boardRequestStatus: ResponseState = ResponseState.loading;
 
@@ -63,12 +70,15 @@ $board-description-color: #ffa41b;
 $board-hover-color: #e7b2a5;
 
 .board-list-container {
-    text-align: left;
+  //padding: 40px 80px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
 .board-link {
     .board-name {
-        display: inline;
+        //display: inline;
         color: $board-name-color;
         cursor: pointer;
     }
@@ -78,7 +88,7 @@ $board-hover-color: #e7b2a5;
     }
 
     .board-description {
-        display: inline;
+        //display: inline;
         color: $board-description-color;
         cursor: pointer;
     }
--- SocialNetwork.WebUI/src/views/MainView.vue
+++ SocialNetwork.WebUI/src/views/MainView.vue
@@ -12,7 +12,7 @@ import BoardListComponent from '@/components/BoardListComponent.vue';
 
 @Component({
   components: { 
-    BoardListComponent
+    BoardListComponent,
   }
 })
 export default class MainView extends Vue {
@@ -23,13 +23,12 @@ export default class MainView extends Vue {
 <style lang="scss" scoped>
 .main-view {
   text-align: center;
-  position: absolute;
-  width: 30%;
-  min-width: 30%;
+  position: relative;
+  width: 100%;
+  height: 100%;
   top: 15%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
+  //-webkit-transform: translate(-50%, -50%);
+  //transform: translate(-50%, -50%);
   color: white;
   font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
   h1 {
