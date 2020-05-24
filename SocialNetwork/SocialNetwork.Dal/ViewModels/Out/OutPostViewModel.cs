62caff115299a616d89cc8332a860d9a607ca691
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-05-24 18:01:00 +0300

    Update OutPostViewModel.cs

--- SocialNetwork/SocialNetwork.Dal/ViewModels/Out/OutPostViewModel.cs
+++ SocialNetwork/SocialNetwork.Dal/ViewModels/Out/OutPostViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using SocialNetwork.Dal.Models;
+using System;
 using System.Collections.Generic;
-using System.Numerics;
-using SocialNetwork.Dal.Models;
 
 namespace SocialNetwork.Dal.ViewModels.Out
 {
@@ -11,6 +10,7 @@ namespace SocialNetwork.Dal.ViewModels.Out
         public string Text { get; set; }
         public string Title { get; set; }
         public Guid UserId { get; set; }
+        public Guid BoardId { get; set; }
         public List<OutAttachmentViewModel> AttachmentPost { get; set; }
         public List<OutMentionViewModel> Mention { get; set; }
     }
