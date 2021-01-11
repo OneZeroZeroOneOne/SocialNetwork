22569469abbe0259b94cf0a69e373d8b2e88d60b
Forevka <32968153+Forevka@users.noreply.github.com>
2021-01-11 22:27:07 +0200

    Update ApiClient.ts

--- SocialNetwork.WebUI/src/services/Implementations/ApiClient.ts
+++ SocialNetwork.WebUI/src/services/Implementations/ApiClient.ts
@@ -6,7 +6,7 @@ class ApiClient {
 
     constructor() {
         this._axiosClient = axios.create({
-            baseURL: 'http://194.99.21.140/api/'
+            baseURL: 'https://194.99.21.140/api/'
         })
         this._axiosClient.interceptors.response.use((response) => this.authTokenExpiredInterceptor(response))
     }
